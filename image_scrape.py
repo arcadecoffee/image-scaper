1d63e42e1897bf04ad427b6677e04c82387b5947
Jerry Gregoire <jerry@arcadecoffee.com>
2022-09-01 14:51:07 -0500

    tweaked for bring a trailer listings

--- image_scrape.py
+++ image_scrape.py
@@ -1,20 +1,36 @@
-import json, requests, shutil
+import html
+import json
+import re
+import shutil
+import sys
 
 from pathlib import Path
-Path("./images").mkdir(parents=True, exist_ok=True)
 
-def main():
-    with open('data.json') as f:
-        json_data = json.load(f)
-        for item in json_data['RelaProperty']['imageListPS']:
-            url = item['url']
-            filename = url.split('/')[-1]
-            r = requests.get(url, stream=True)
-            print(f'getting {url}')
-            if r.status_code == 200:
-                with open(f'images/{filename}', 'wb') as of:
-                    r.raw.decode_content = True
-                    shutil.copyfileobj(r.raw, of)
+import requests
+
+
+def main(url):
+    content = requests.get(url).content.decode()
+    gallery = json.loads(
+        html.unescape(
+            re.search(r'<div data-gallery-items="([^ ]*)" ', content)[1]
+        )
+    )
+
+    path = "./images/" + re.sub(r'http.*:\/\/', '', url).replace('/', '_')
+    Path(path).mkdir(parents=True, exist_ok=True)
+
+    for item in gallery:
+        image_url = item['large']['url'].split('?')[0]
+        image_filename = image_url.split('/')[-1]
+
+        r = requests.get(image_url, stream=True)
+        print(f'getting {image_url}')
+        if r.status_code == 200:
+            with open(f'{path}/{image_filename}', 'wb') as of:
+                r.raw.decode_content = True
+                shutil.copyfileobj(r.raw, of)
+
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1])
